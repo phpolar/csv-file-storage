5e0cf76f007bce805f6b81a6d6aca5f273b6f8ee
Eric Fortmeyer <efortmeyer@bethel.jw.org>
2023-02-09 20:27:20 -0600

    docs: add api docs

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,44 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Phpolar\\CsvFileStorage\\CsvFileStorage",
+            "name": "CsvFileStorage",
+            "summary": "Allows\u0020for\u0020saving\u0020data\u0020to\u0020a\u0020CSV\u0020file.",
+            "url": "classes/Phpolar-CsvFileStorage-CsvFileStorage.html"
+        },                {
+            "fqsen": "\\Phpolar\\CsvFileStorage\\CsvFileStorage\u003A\u003A__construct\u0028\u0029",
+            "name": "__construct",
+            "summary": "",
+            "url": "classes/Phpolar-CsvFileStorage-CsvFileStorage.html#method___construct"
+        },                {
+            "fqsen": "\\Phpolar\\CsvFileStorage\\CsvFileStorage\u003A\u003A__destruct\u0028\u0029",
+            "name": "__destruct",
+            "summary": "",
+            "url": "classes/Phpolar-CsvFileStorage-CsvFileStorage.html#method___destruct"
+        },                {
+            "fqsen": "\\Phpolar\\CsvFileStorage\\CsvFileStorage\u003A\u003Acommit\u0028\u0029",
+            "name": "commit",
+            "summary": "",
+            "url": "classes/Phpolar-CsvFileStorage-CsvFileStorage.html#method_commit"
+        },                {
+            "fqsen": "\\Phpolar\\CsvFileStorage\\CsvFileStorage\u003A\u003Aload\u0028\u0029",
+            "name": "load",
+            "summary": "",
+            "url": "classes/Phpolar-CsvFileStorage-CsvFileStorage.html#method_load"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\Phpolar\\CsvFileStorage",
+            "name": "CsvFileStorage",
+            "summary": "",
+            "url": "namespaces/phpolar-csvfilestorage.html"
+        },                {
+            "fqsen": "\\Phpolar",
+            "name": "Phpolar",
+            "summary": "",
+            "url": "namespaces/phpolar.html"
+        }            ]
+);
